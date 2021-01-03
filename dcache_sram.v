bbf300ef43c8a06d94837d2cf5b5aebede1ba71f
Sophia Huang <b07902031@csie.ntu.edu.tw>
2021-01-03 21:14:16 +0800

    modify tag and make the code clear in sram

--- dcache_sram.v
+++ dcache_sram.v
@@ -30,48 +30,36 @@ output             hit_o;
 reg      [24:0]    tag [0:15][0:1];    
 reg      [255:0]   data[0:15][0:1];
 reg      last[0:15];
-reg      isFilled[0:15][0:1];
 
 integer            i, j;
+
+wire block_hit[0:1];
 
 // Write Data      
 // 1. Write hit
 // 2. Read miss: Read from memory
-always@(posedge clk_i or posedge rst_i) begin
+always @(posedge clk_i or posedge rst_i) begin
     if (rst_i) begin
         for (i=0;i<16;i=i+1) begin
             for (j=0;j<2;j=j+1) begin
                 tag[i][j] <= 25'b0;
                 data[i][j] <= 256'b0;
-                isFilled[i][j] <= 1'b0;
             end
         end
     end
     if (enable_i && write_i) begin
         // TODO: Handle your write of 2-way associative cache + LRU here
-        if (isFilled[addr_i][0] == 0) begin //block 0 is available to use
+        if (last[addr_i] == 1'b1) begin
             data[addr_i][0] <= data_i;
-            tag[addr_i][0] <= tag_i;
-            last[addr_i] <= 0;
-            isFilled[addr_i][0] <= 1;
+            tag[addr_i][0][22:0] <= tag_i[22:0];
+            tag[addr_i][0][24:23] <= 2'b1;
+            last[addr_i] <= 1'b0;
         end
-        else if (isFilled[addr_i][1] == 0) begin //block 1 is available to use
+        else begin
             data[addr_i][1] <= data_i;
-            tag[addr_i][1] <= tag_i;
-            last[addr_i] <= 1;
-            isFilled[addr_i][1] <= 1;
-        end
-        else begin //block 0 and 1 are not available to use => LRU
-            if (last[addr_i] == 1) begin
-                data[addr_i][0] <= data_i;
-                tag[addr_i][0] <= tag_i;
-                last[addr_i] <= 0;
-            end
-            else begin
-                data[addr_i][1] <= data_i;
-                tag[addr_i][1] <= tag_i;
-                last[addr_i] <= 1;
-            end
+            tag[addr_i][1][22:0] <= tag_i[22:0];
+            tag[addr_i][1][24:23] <= 2'b1;
+            last[addr_i] <= 1'b1;
         end
     end
 end
@@ -79,13 +67,18 @@ end
 // Read Data      
 // TODO: tag_o=? data_o=? hit_o=?
 
+assign block_hit[0] = ((tag[addr_i][0][24] == 1) && (tag_i[22:0] == tag[addr_i][0][22:0])) ? 1'b1 : 1'b0;
+assign block_hit[1] = ((tag[addr_i][1][24] == 1) && (tag_i[22:0] == tag[addr_i][1][22:0])) ? 1'b1 : 1'b0;
+
 assign data_o = (enable_i == 0) ? 256'b0 : 
-        ((tag_i[24] == 1 && tag[addr_i][0][24] == 1 && tag_i[22:0] == tag[addr_i][0]) ? data[addr_i][0] : // valid bit of the input == 1 and tag of input == tag address for block 0
-            ((tag_i[24] == 1 && tag[addr_i][1][24] == 1 && tag_i[22:0] == tag[addr_i][1]) ? data[addr_i][1] : 256'b0)); // valid bit of the input == 1 and tag of input == tag address for block 1
+        (block_hit[0] == 1) ? data[addr_i][0] : 
+        (block_hit[1] == 1) ? data[addr_i][1] : 256'b0;
+
 assign tag_o = (enable_i == 0) ? 25'b0 :
-        ((tag_i[24] == 1 && tag[addr_i][0][24] == 1 && tag_i[22:0] == tag[addr_i][0]) ? tag[addr_i][0] : // valid bit of the input == 1 and tag of input == tag address for block 0
-            ((tag_i[24] == 1 && tag[addr_i][1][24] == 1 && tag_i[22:0] == tag[addr_i][1]) ? tag[addr_i][1] : 25'b0)); // valid bit of the input == 1 and tag of input == tag address for block 1
+        (block_hit[0] == 1) ? tag[addr_i][0] : 
+        (block_hit[1] == 1) ? tag[addr_i][1] : 25'b0;
+
 assign hit_o = (enable_i == 0) ? 1'b0 :
-        (((tag_i[24] == 1 && tag[addr_i][0][24] == 1 && tag_i[22:0] == tag[addr_i][0]) || (tag_i[24] == 1 && tag[addr_i][1][24] == 1 && tag_i[22:0] == tag[addr_i][1])) ? 1'b1 : 1'b0);
+        ((block_hit[0] == 1) || (block_hit[1] == 1)) ? 1'b1 : 1'b0;
 
 endmodule
