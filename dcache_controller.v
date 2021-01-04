96da7d0f20faa92768b62f6b269fdf995e5d70e9
Sophia Huang <b07902031@csie.ntu.edu.tw>
2021-01-04 16:16:36 +0800

    fix some bug of hit

--- dcache_controller.v
+++ dcache_controller.v
@@ -118,8 +118,8 @@ assign    write_hit    = hit & cpu_MemWrite_i;
 assign    cache_dirty  = write_hit;
 
 // TODO: add your code here!  (r_hit_data=...?)
-assign hit = sram_valid & (cpu_tag == sram_cache_tag[21:0]);
-assign r_hit_data = sram_cache_data;
+assign hit = sram_valid & (cpu_tag == sram_cache_tag[22:0]);
+assign r_hit_data = (hit) ? sram_cache_data : mem_data_i;
 // read data :  256-bit to 32-bit
 always@(cpu_offset or r_hit_data) begin
     // TODO: add your code here! (cpu_data=...?)
